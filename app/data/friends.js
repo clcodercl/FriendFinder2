fd9643671c504d6e9f925683be5019381bdd2f18
cynthe@sbcglobal.net <cynthecoder@gmail.com>
2016-12-29 20:44:05 -0600

    add

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,20 @@
 var friends = [{ 
-	'name': "Ashley"
-	'photo': 'http://1.bp.blogspot.com/-RjKFcSir25s/Ts_Qokg1_fI/AAAAAAAAAk0/UdF-pBinTCY/s1600/ashley-greene.jpg'
+	'name': "Keely"
+	'photo': 'http://www.skinet.com/warrenmiller/files/imagecache/gallery_2015/_images/201305/mt_pascal-beauvais_dsc_0696.jpg'
 	'scores': [2, 1, 3, 5, 4, 5, 3, 2, 5, 1]
-}, {}
+}, {
+	'name': "Julie"
+	'photo': 'http://www.pentaxforums.com/forums/attachments/12-post-your-photos/107656d1320130691-people-random-people-portraits-imgp0948.jpg'
+	'scores': [4, 5, 3, 2, 1, 4, 2, 4, 5, 2]
+}, {
+	'name': "Octavia"
+	'photo': 'http://ofad.org/files/daily-photo/recent-portraits-random-people-in-random-places_3.jpg'
+	'scores': [3, 2, 2, 1, 4, 5, 2, 3, 5, 3]
+}, {
+	'name': "Alexa"
+	'photo': 'http://ofad.org/files/daily-photo/recent-portraits-random-people-in-random-places_3.jpg'
+	'scores': [1, 2, 2, 4, 3, 3, 5, 4, 2, 5]
+
+
+// export the array. 
+module.exports = tableArray;
