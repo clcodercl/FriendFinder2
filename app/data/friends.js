7581f3e2702410083770d4234f42b136f5ac4f35
cynthe@sbcglobal.net <cynthecoder@gmail.com>
2016-12-29 19:34:06 -0600

    add

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+var friends = [{ 
+	'name': "Ashley"
+	'photo': 'http://1.bp.blogspot.com/-RjKFcSir25s/Ts_Qokg1_fI/AAAAAAAAAk0/UdF-pBinTCY/s1600/ashley-greene.jpg'
+	'scores': [2, 1, 3, 5, 4, 5, 3, 2, 5, 1]
+}, {}
