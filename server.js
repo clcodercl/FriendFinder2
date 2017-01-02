80bb7e9dde2b404680a52e0eee9104cfb05a20a3
cynthe@sbcglobal.net <cynthecoder@gmail.com>
2017-01-02 06:05:59 -0600

    adding express

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express(); // Tells node that we are creating an "express" server
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(__dirname +'/app/public/'));
+
+
+
+
+app.listen(PORT, function () {
+	console.log('App listening on PORT ' + PORT);
+});
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+// ================================================================================
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
